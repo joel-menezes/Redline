8128379f3545f934c6c812f7bffe015e76f080c1
joel-menezes <smartjoel123@gmail.com>
2026-09-18 23:33:28 -0400

    Loaded .env Variables

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import dropbox
 from obsws_python.error import OBSSDKError, OBSSDKRequestError
 import os
 
+
+HOST = "localhost"
+PORT = 4455
+PASSWORD = os.getenv("obs_password")
+DBXPASSWORD = os.getenv("app_key")
+app_key = os.getenv("app_secret")
+app_secret = os.getenv("authorization_code")
+authorization_code = os.getenv("refresh_token")
+refresh_token = os.getenv("DBXPASSWORD")
